8957914fb0c90c5ab72e8dbdc4093ab11b98b2c7
amo0114 <108148125+amo0114@users.noreply.github.com>
2025-09-01 17:10:43 +0800

    Update vite.config.ts

--- vite-frontend/vite.config.ts
+++ vite-frontend/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import legacy from '@vitejs/plugin-legacy'
+// import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   plugins: [
     react(),
-    legacy({
-      targets: ['defaults', 'not IE 11']
-    })
+    // legacy({
+    //   targets: ['defaults', 'not IE 11']
+    // })
   ],
   base: './',    
   resolve: {
